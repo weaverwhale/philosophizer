f92cda3640205c301d2c4c2a2c2d1e7429ab707d
mjweaver01 <8439630+mjweaver01@users.noreply.github.com>
2025-12-30 18:50:44 -0500

    Dark mode logo

--- src/frontend/components/Logo.tsx
+++ src/frontend/components/Logo.tsx
@@ -2,7 +2,11 @@ import phiLogo from '../phi.svg';
 
 export function Logo() {
   return (
-    <img src={phiLogo} alt="Philosophizer" className="w-16 h-16 mx-auto mb-4" />
+    <img
+      src={phiLogo}
+      alt="Philosophizer"
+      className="logo-image w-16 h-16 mx-auto mb-4"
+    />
   );
 }
 
